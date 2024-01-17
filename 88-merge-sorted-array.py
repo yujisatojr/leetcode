16f416892b425266b009605a5ad7fa65193b32ee
Yuji Sato <yujisatojr@gmail.com>
2024-01-17 16:50:33 -0600

    Initial commit

---
+++ 88-merge-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+
+        i = n + m - 1 # nums1 index
+        j = n - 1 #nums2 index
+
+        while j >= 0:
+            nums1[i] = nums2[j]
+            i -= 1
+            j -= 1
+
+        nums1.sort()
